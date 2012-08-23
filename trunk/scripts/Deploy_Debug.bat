28c5cb54bb29a4f5f6daecf3d9b57702244b166d
yoavain@gmail.com <yoavain@gmail.com@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2012-08-23 15:14:54 +0000

    Fix scripts

--- trunk/scripts/Deploy_Debug.bat
+++ trunk/scripts/Deploy_Debug.bat
@@ -11,8 +11,8 @@ if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
 	set PROGS=%ProgramFiles%	
 :CONT
 
-copy /y source\My Lyrics\bin\debug\LyricsEngine.dll "%PROGS%\Team MediaPortal\MediaPortal\"
-copy /y source\My Lyrics\bin\debug\TranslateProvider.dll "%PROGS%\Team MediaPortal\MediaPortal\"
-copy /y source\My Lyrics\bin\debug\MyLyrics.dll "%PROGS%\Team MediaPortal\MediaPortal\plugins\Windows\"
+copy /y "source\My Lyrics\bin\debug\LyricsEngine.dll" "%PROGS%\Team MediaPortal\MediaPortal\"
+copy /y "source\My Lyrics\bin\debug\TranslateProvider.dll" "%PROGS%\Team MediaPortal\MediaPortal\"
+copy /y "source\My Lyrics\bin\debug\MyLyrics.dll" "%PROGS%\Team MediaPortal\MediaPortal\plugins\Windows\"
 
 cd scripts
