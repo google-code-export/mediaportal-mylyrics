71f5cc3ef50c9f2ca51d079eaf99f6b47237e420
yoavain@gmail.com <yoavain@gmail.com@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2012-08-26 17:36:38 +0000

    Added test for Lyrics007 to test "U2 - Staring At The Sun", just to make sure

--- source/MyLyricsTests/MyLyricsLyrics007Test.cs
+++ source/MyLyricsTests/MyLyricsLyrics007Test.cs
@@ -5,25 +5,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MyLyricsTests
 {
-    [TestClass]
-    public class MyLyricsLyrics007Test
+  [TestClass]
+  public class MyLyricsLyrics007Test
+  {
+    [TestMethod]
+    public void TestLyrics007()
     {
-        [TestMethod]
-        public void TestLyrics007()
-        {
-          var u2WithOrWithoutYou = new Lyrics007("U2", "With Or Without You", new ManualResetEvent(false), 10000);
-            var splitMam = u2WithOrWithoutYou.Lyric.Split(' ');
-            Assert.AreEqual("See", splitMam[0]);
-            Assert.AreEqual("you", splitMam[splitMam.Length - 1]);
-        }
+      var u2WithOrWithoutYou = new Lyrics007("U2", "With Or Without You", new ManualResetEvent(false), 10000);
+      var splitUwowy = u2WithOrWithoutYou.Lyric.Split(' ');
+      Assert.AreEqual("See", splitUwowy[0]);
+      Assert.AreEqual("you", splitUwowy[splitUwowy.Length - 1]);
 
-        [TestMethod]
-        public void TestLyrics007NotFound()
-        {
-            var notFound = new Lyrics007("Foo", "Bar", new ManualResetEvent(false), 10000);
-            var splitMam = notFound.Lyric.Split(' ');
-            Assert.AreEqual("Not", splitMam[0]);
-            Assert.AreEqual("found", splitMam[splitMam.Length - 1]);
-        }
+      var u2StaringAtTheSun = new Lyrics007("U2", "Staring At The Sun", new ManualResetEvent(false), 10000);
+      var splitUsats = u2StaringAtTheSun.Lyric.Split(' ');
+      Assert.AreEqual("Staring", splitUsats[0]);
+      Assert.AreEqual("blind", splitUsats[splitUsats.Length - 1]);
     }
+
+    [TestMethod]
+    public void TestLyrics007NotFound()
+    {
+      var notFound = new Lyrics007("Foo", "Bar", new ManualResetEvent(false), 10000);
+      var splitNf = notFound.Lyric.Split(' ');
+      Assert.AreEqual("Not", splitNf[0]);
+      Assert.AreEqual("found", splitNf[splitNf.Length - 1]);
+    }
+  }
 }
