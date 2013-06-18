547da03639eaaa43ed06aa7de2e194b4b7a478ad
yoavain@gmail.com <yoavain@gmail.com@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2013-06-18 15:19:18 +0000

    Merge changes from 1.4 branch

--- trunk/source/TranslateProvider/Properties/AssemblyInfo.cs
+++ trunk/source/TranslateProvider/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TranslateProvider")]
-[assembly: AssemblyCopyright("Copyright © 2009-2012")]
+[assembly: AssemblyCopyright("Copyright © 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.7.0.$WCREV$")]
-[assembly: AssemblyFileVersion("1.7.0.$WCREV$")]
+[assembly: AssemblyVersion("1.7.1.$WCREV$")]
+[assembly: AssemblyFileVersion("1.7.1.$WCREV$")]
