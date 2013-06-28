cf22b01a91f930d402cf0cb413dfec5a512d0e29
yoavain <yoavain@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2013-06-28 21:28:03 +0000

    Fix lyrdb.com (test still fails if timeouts)

---
+++ source/MyLyricsTests/MyLyricsLyrDbTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using LyricsEngine.LyricsSites;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLyricsTests
+{
+    [TestClass]
+    public class MyLyricsLyrDbTest
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        [TestMethod]
+        public void TestLyrDb()
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            var withOrWithoutYou = new LyrDB("U2", "With Or Without You", new ManualResetEvent(false), 120000);
+            _stopwatch.Stop();
+            Debug.WriteLine("Test duration: " + _stopwatch.Elapsed);
+            var splitMam = withOrWithoutYou.Lyric.Split(' ');
+            Assert.AreEqual("See", splitMam[0]);
+            Assert.AreEqual("you", splitMam[splitMam.Length - 1]);
+        }
+
+        [TestMethod]
+        public void TestLyrDbNotFound()
+        {
+            var notFound = new LyrDB("Foo", "Bar", new ManualResetEvent(false), 120000);
+            var splitNf = notFound.Lyric.Split(' ');
+            Assert.AreEqual("Not", splitNf[0]);
+            Assert.AreEqual("found", splitNf[splitNf.Length - 1]);
+        }
+    }
+}
