041c49dd4ecb4d3a6b562abb37734a99649ab67f
yoavain <yoavain@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2013-07-03 05:16:54 +0000

    Added interface for lyrics site with an Abstract Site class

---
+++ trunk/source/LyricsEngine/LyricsSites/ILyricSite.cs
@@ -0,0 +1,36 @@
+﻿
+namespace LyricsEngine.LyricsSites
+{
+    public enum LyricType
+    {
+        Lrc = 0,
+        UnsyncedLyrics = 1
+    }
+
+    public enum SiteType
+    {
+        Api = 0,
+        Scrapper = 1
+    }
+
+    public enum SiteComplexity
+    {
+        OneStep = 1,
+        TwoSteps = 2
+    }
+
+    interface ILyricSite
+    {
+        string Name { get; }
+
+        void FindLyrics();
+
+        string Lyric { get; }
+
+        LyricType GetLyricType();
+
+        SiteType GetSiteType();
+
+        SiteComplexity GetSiteComplexity();
+    }
+}
