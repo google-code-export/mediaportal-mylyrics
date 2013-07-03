682853711748545ecaf72f81b3a273a33f11c54c
yoavain <yoavain@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2013-07-03 05:21:54 +0000

    Moved Shironet to extend AbstractSite (that implements site interface)

--- trunk/source/MyLyricsTests/MyLyricsShironetTest.cs
+++ trunk/source/MyLyricsTests/MyLyricsShironetTest.cs
@@ -12,6 +12,8 @@ namespace MyLyricsTests
         public void TestShironet1()
         {
             var mashinaAtidMatok = new Shironet("משינה", "עתיד מתוק", new ManualResetEvent(false), 10000);
+            //Thread.Sleep(15000);
+            mashinaAtidMatok.FindLyrics();
             var splitMam = mashinaAtidMatok.Lyric.Split(' ');
             Assert.AreEqual("כולם", splitMam[0]);
             Assert.AreEqual("טוב...", splitMam[splitMam.Length - 1]);
@@ -21,6 +23,7 @@ namespace MyLyricsTests
         public void TestShironet2()
         {
             var shabakSamehNofelVekam = new Shironet("שבק ס", "נופל וקם", new ManualResetEvent(false), 10000);
+            shabakSamehNofelVekam.FindLyrics();
             var splitSsnv = shabakSamehNofelVekam.Lyric.Split(' ');
             Assert.AreEqual("הדרך", splitSsnv[0]);
             Assert.AreEqual("ומפותלת...", splitSsnv[splitSsnv.Length - 1]);
@@ -30,6 +33,7 @@ namespace MyLyricsTests
         public void TestShironet3()
         {
             var erikBermanJetLag = new Shironet("אריק ברמן", "ג'ט לג", new ManualResetEvent(false), 10000);
+            erikBermanJetLag.FindLyrics();
             var splitEbjl = erikBermanJetLag.Lyric.Split(' ');
             Assert.AreEqual("אל", splitEbjl[0]);
             Assert.AreEqual("לסדין.", splitEbjl[splitEbjl.Length - 1]);
@@ -39,6 +43,7 @@ namespace MyLyricsTests
         public void TestShironet4()
         {
             var efratGoshTamid = new Shironet("אפרת גוש", "תמיד כשאתה בא", new ManualResetEvent(false), 10000);
+            efratGoshTamid.FindLyrics();
             var splitEbjl = efratGoshTamid.Lyric.Split(' ');
             Assert.AreEqual("תמיד", splitEbjl[0]);
             Assert.AreEqual("באמת", splitEbjl[splitEbjl.Length - 1]);
@@ -51,6 +56,7 @@ namespace MyLyricsTests
             {
                 Debug.WriteLine(string.Format("Iteration {0}", i));
                 var mashinaAtidMatok = new Shironet("משינה", "עתיד מתוק", new ManualResetEvent(false), 3000);
+                mashinaAtidMatok.FindLyrics();
                 var splitMam = mashinaAtidMatok.Lyric.Split(' ');
                 Assert.AreEqual("כולם", splitMam[0]);
                 Assert.AreEqual("טוב...", splitMam[splitMam.Length - 1]);
@@ -61,6 +67,7 @@ namespace MyLyricsTests
         public void TestShironetNotFound()
         {
             var notFound = new Shironet("Foo", "Bar", new ManualResetEvent(false), 10000);
+            notFound.FindLyrics();
             var splitNf = notFound.Lyric.Split(' ');
             Assert.AreEqual("Not", splitNf[0]);
             Assert.AreEqual("found", splitNf[splitNf.Length - 1]);
