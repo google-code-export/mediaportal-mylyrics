03fa181fe24309d1f7ef79040369ce30932bcb72
yoavain <yoavain@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2013-07-03 19:23:12 +0000

    Added test for LyricWiki (fails)

---
+++ trunk/source/MyLyricsTests/MyLyricsLyricWikiTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using LyricsEngine.LyricsSites;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLyricsTests
+{
+    [TestClass]
+    public class MyLyricsLyricWikiTest
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _stopwatch.Stop();
+            Debug.WriteLine("Test duration: " + _stopwatch.Elapsed);
+        }
+
+        [TestMethod]
+        public void TestLyricWiki()
+        {
+            var withOrWithoutYou = new LyricWiki("U2", "With Or Without You", new ManualResetEvent(false), 300000);
+            withOrWithoutYou.FindLyrics();
+            var splitMam = withOrWithoutYou.Lyric.Split(' ');
+            Assert.AreEqual("See", splitMam[0]);
+            Assert.AreEqual("you", splitMam[splitMam.Length - 1]);
+        }
+
+        [TestMethod]
+        public void TestLyricWikiNotFound()
+        {
+            var notFound = new LyricWiki("Foo", "Bar", new ManualResetEvent(false), 30000);
+            notFound.FindLyrics();
+            var splitNf = notFound.Lyric.Split(' ');
+            Assert.AreEqual("Not", splitNf[0]);
+            Assert.AreEqual("found", splitNf[splitNf.Length - 1]);
+        }
+    }
+}
