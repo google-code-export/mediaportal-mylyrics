c5d23c67c848e3cb774f93d0b710d246c65638b7
yoavain <yoavain@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2013-07-03 19:27:30 +0000

    Added stopwatch to all tests

--- source/MyLyricsTests/MyLyricsLyrics007Test.cs
+++ source/MyLyricsTests/MyLyricsLyrics007Test.cs
@@ -1,33 +1,51 @@
-﻿using System.Threading;
+﻿using System.Diagnostics;
+using System.Threading;
 using LyricsEngine.LyricsSites;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MyLyricsTests
 {
-  [TestClass]
-  public class MyLyricsLyrics007Test
-  {
-    [TestMethod]
-    public void TestLyrics007()
+    [TestClass]
+    public class MyLyricsLyrics007Test
     {
-      var u2WithOrWithoutYou = new Lyrics007("U2", "With Or Without You", new ManualResetEvent(false), 10000);
-      var splitUwowy = u2WithOrWithoutYou.Lyric.Split(' ');
-      Assert.AreEqual("See", splitUwowy[0]);
-      Assert.AreEqual("you", splitUwowy[splitUwowy.Length - 1]);
+        private readonly Stopwatch _stopwatch = new Stopwatch();
 
-      var u2StaringAtTheSun = new Lyrics007("U2", "Staring At The Sun", new ManualResetEvent(false), 10000);
-      var splitUsats = u2StaringAtTheSun.Lyric.Split(' ');
-      Assert.AreEqual("Summer", splitUsats[0]);
-      Assert.AreEqual("I", splitUsats[splitUsats.Length - 1]);
-    }
+        [TestInitialize]
+        public void SetUp()
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+        }
 
-    [TestMethod]
-    public void TestLyrics007NotFound()
-    {
-      var notFound = new Lyrics007("Foo", "Bar", new ManualResetEvent(false), 10000);
-      var splitNf = notFound.Lyric.Split(' ');
-      Assert.AreEqual("Not", splitNf[0]);
-      Assert.AreEqual("found", splitNf[splitNf.Length - 1]);
+        [TestCleanup]
+        public void TearDown()
+        {
+            _stopwatch.Stop();
+            Debug.WriteLine("Test duration: " + _stopwatch.Elapsed);
+        }
+
+
+        [TestMethod]
+        public void TestLyrics007()
+        {
+            var u2WithOrWithoutYou = new Lyrics007("U2", "With Or Without You", new ManualResetEvent(false), 10000);
+            var splitUwowy = u2WithOrWithoutYou.Lyric.Split(' ');
+            Assert.AreEqual("See", splitUwowy[0]);
+            Assert.AreEqual("you", splitUwowy[splitUwowy.Length - 1]);
+
+            var u2StaringAtTheSun = new Lyrics007("U2", "Staring At The Sun", new ManualResetEvent(false), 10000);
+            var splitUsats = u2StaringAtTheSun.Lyric.Split(' ');
+            Assert.AreEqual("Summer", splitUsats[0]);
+            Assert.AreEqual("I", splitUsats[splitUsats.Length - 1]);
+        }
+
+        [TestMethod]
+        public void TestLyrics007NotFound()
+        {
+            var notFound = new Lyrics007("Foo", "Bar", new ManualResetEvent(false), 10000);
+            var splitNf = notFound.Lyric.Split(' ');
+            Assert.AreEqual("Not", splitNf[0]);
+            Assert.AreEqual("found", splitNf[splitNf.Length - 1]);
+        }
     }
-  }
 }
