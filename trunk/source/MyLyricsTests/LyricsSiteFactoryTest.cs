0b906204f718c3ba5ffafe5d15882b9bb7ae011b
yoavain <yoavain@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2013-07-04 23:15:02 +0000

    Created a LyricsSite factory class (+test)

---
+++ trunk/source/MyLyricsTests/LyricsSiteFactoryTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using LyricsEngine.LyricsSites;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLyricsTests
+{
+    [TestClass]
+    public class LyricsSiteFactoryTest
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _stopwatch.Stop();
+            Debug.WriteLine("Test duration: " + _stopwatch.Elapsed);
+        }
+
+
+        [TestMethod]
+        public void TestLyricsSiteFactory()
+        {
+            var lyricsSitesNames = LyricsSiteFactory.LyricsSitesNames();
+            foreach (var lyricsSitesName in lyricsSitesNames)
+            {
+                Debug.WriteLine("Executing lyrics site " + lyricsSitesName);
+                var abstractSite = LyricsSiteFactory.Create(lyricsSitesName, "U2", "With Or Without You", new ManualResetEvent(false), 5000);
+                abstractSite.FindLyrics();
+                // No assertions
+            }
+        }
+    }
+}
