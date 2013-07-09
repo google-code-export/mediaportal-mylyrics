abc6b41e238e432bf4cda8b97fc00769c7208a77
yoavain <yoavain@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2013-07-09 17:32:10 +0000

    Fix search within the GUI (use new interface to tell between LRC and non-LRC sites)

--- source/LyricsEngine/Setup.cs
+++ source/LyricsEngine/Setup.cs
@@ -4,21 +4,13 @@ namespace LyricsEngine
 {
     public static class Setup
     {
-        public static List<string> AllLyricsSites = new List<string>();
-
         static Setup()
         {
             ActiveSites = new List<string>();
         }
 
         public static List<string> ActiveSites { get; set; }
-
-
-        public static List<string> AllSites()
-        {
-            return AllLyricsSites;
-        }
-
+        
         public static bool IsMember(string site)
         {
             return ActiveSites.Contains(site);
