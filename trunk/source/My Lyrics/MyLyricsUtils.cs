3fb784c9fe98e9b2dfda7a07dc524d734ba0571f
yoavain <yoavain@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2013-07-11 22:28:41 +0000

    Missing file

---
+++ trunk/source/My Lyrics/MyLyricsUtils.cs
@@ -0,0 +1,21 @@
+﻿namespace MyLyrics
+{
+    public class MyLyricsUtils
+    {
+        // Old Settings filename
+        public static readonly string OldSettingsFileName = "MediaPortal.xml";
+
+        // New Settings filename
+        public static readonly string SettingsFileName = "MyLyrics.xml";
+
+        // Log filename
+        public static string LogFileName = "MyLyrics.log";
+        public static string OldLogFileName = "MyLyrics.log.bak";
+
+        // Database settings
+        internal static LyricsDatabase LyricsDB;
+        internal static string LyricsDBName = "LyricsDatabaseV2.db";
+        internal static LyricsDatabase LyricsMarkedDB;
+        internal static string LyricsMarkedDBName = "LyricsMarkedDatabaseV2.db";
+    }
+}
