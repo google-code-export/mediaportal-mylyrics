746e6d76e157a2d0af08ac015df9fbd733f783aa
yoavain <yoavain@595c4c07-e269-cbe5-8599-9ea3db5898ed>
2014-01-04 22:48:36 +0000

    Welcome 2014

--- trunk/source/TranslateProvider/Properties/AssemblyInfo.cs
+++ trunk/source/TranslateProvider/Properties/AssemblyInfo.cs
@@ -10,18 +10,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TranslateProvider")]
-[assembly: AssemblyCopyright("Copyright © 2009-2013")]
+[assembly: AssemblyCopyright("Copyright © 2009-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("6ceaa1b8-dd2d-40db-933d-b18468b4e960")]
 
 // Version information for an assembly consists of the following four values:
@@ -34,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.9.0.$WCREV$")]
 [assembly: AssemblyFileVersion("1.9.0.$WCREV$")]
